727d0d3eac76b4855e23f8aa4d7447dd7d374f6d
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-07-14 20:23:44 -0300

    add user tests

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,13 +1,13 @@
 import chaiHttp from 'chai-http';
 import chai from 'chai';
 import app from '../index'
-//import request from 'supertest';
-import { provideToken, verifyToken } from '../libs/jwt';
+import { provideToken } from '../libs/jwt';
 
 const assert = chai.assert;
 chai.use(chaiHttp);
 
 let userId: String;
+let confirmToken: String;
 let token: String;
 
 describe("POST /auth/register", () => {
@@ -24,7 +24,7 @@ describe("POST /auth/register", () => {
             .send(userData)
             .end(function (err, res) {
                 userId = res.body.data.id;
-                token = provideToken(res.body.data.id);
+                confirmToken = provideToken(res.body.data.id);
                 assert.equal(res.status, 201);
                 assert.equal(res.body.success, true);
                 done();
@@ -102,15 +102,26 @@ describe("POST /auth/register", () => {
 
 describe('Confirm email', () => {
 
-    it('confirm email with valid token', (done) => {
+    it('confirm email with valid confirmToken', (done) => {
         chai.request(app)
-            .get(`/auth/confirmation/${token}`)
+            .get(`/auth/confirmation/${confirmToken}`)
             .end(function (err, res) {
                 assert.equal(res.status, 200);
                 assert.equal(res.body.success, true);
                 done();
             })
     })
+
+    it('confirm email with invalid confirmToken', (done) => {
+        chai.request(app)
+            .get(`/auth/confirmation/12321321321asd`)
+            .end(function (err, res) {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.success, false);
+                done();
+            })
+    })
+
 });
 
 describe('POST /auth/login', () => {
@@ -129,5 +140,30 @@ describe('POST /auth/login', () => {
                 done();
             })
     });
+});
 
+describe('GET /user/profile', () => {
+    it('get profile with valid userId', (done) => {
+        chai.request(app)
+            .get('/user/profile')
+            .set('Authorization', `Bearer ${token}`)
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.success, true);
+                done();
+            })
+    })
+})
+
+describe('DELETE /user/deleteAccount', () => {
+    it('delete current user account', (done) => {
+        chai.request(app)
+            .delete('/user/deleteAccount')
+            .set('Authorization', `Bearer ${token}`)
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.success, true);
+                done();
+            });
+    });
 });
