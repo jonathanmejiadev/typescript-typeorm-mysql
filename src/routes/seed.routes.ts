464c381cf2bd0d46636e1636fe6f3323a00e04f0
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-08-06 17:07:10 -0300

    Add seed controller

--- src/routes/seed.routes.ts
+++ src/routes/seed.routes.ts
@@ -9,9 +9,12 @@ const seedRouter = Router();
 
 seedRouter.post('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
+        await Promise.all(categories.map((category) => {
+            categoryService.createCategory(category);
+        }));
         await Promise.all(products.map((product) => {
-            productService.createProduct(product)
-        }))
+            productService.createProduct(product);
+        }));
         return res.status(201).json({ success: true, message: 'Seeds loaded successfully' });
     } catch (err) {
         next(err);
